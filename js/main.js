a32421df4d5c2085e6ed74b2e6da7d6b2a4ddd5e
XibalbaM <maelporret@outlook.fr>
2023-02-15 18:01:39 +0100

    Small changes

--- js/main.js
+++ js/main.js
@@ -28,12 +28,10 @@ const clanList = [
 ];
 
 const clanBar = document.getElementsByClassName("clan-bar")[0];
-for (let i = 0; i < 3; i++) {
 
-	clanList.forEach(clan => {
-		const clanImg = document.createElement("img");
-		clanImg.src = `img/clans/${clan}`;
-		clanImg.className = "clan-bar__img";
-		clanBar.appendChild(clanImg);
-	});
-}
+clanList.forEach(clan => {
+	const clanImg = document.createElement("img");
+	clanImg.src = `img/clans/${clan}`;
+	clanImg.className = "clan-bar__img";
+	clanBar.appendChild(clanImg);
+});
